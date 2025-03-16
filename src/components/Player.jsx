c3ef0db634828365e7cb6d52c43244406a47e481
Omer Lande <omerl@log-on.com>
2025-03-16 12:23:17 +0200

    enable to set name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayer, setEnteredPlayer] = useState(null);
+
+  function handleClick() {
+    setEnteredPlayer(playerName.current.value);
+    playerName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayer ?? "player1"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
